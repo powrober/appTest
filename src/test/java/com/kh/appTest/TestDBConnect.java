d19d024b94f39afa66c654e518a80479d20add2b
powrober <powrober89@gmail.com>
2021-03-08 11:44:39 +0900

    2021-03-08 appTest

--- src/test/java/com/kh/appTest/TestDBConnect.java
+++ src/test/java/com/kh/appTest/TestDBConnect.java
@@ -16,28 +16,28 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/root-context.xml"})
 public class TestDBConnect {
-	private static final Logger logger = LoggerFactory.getLogger(TestDBConnect.class);
-	// Logger : 로그 출력을 위한 log4j 객체
-	
-	@Autowired
-	private ApplicationContext wac;
-	
-	@Before
-	public void beforeClass() {
-		logger.info("-----테스트 시작-----");		
-	}
-	
-	@After
-	public void afterClass() {
-		logger.info("-----테스트 종료-----");
-	}
+   private static final Logger logger = LoggerFactory.getLogger(TestDBConnect.class);
+   // Logger : 로그 출력을 위한 log4j 객체
+   
+   @Autowired
+   private ApplicationContext wac;
+   
+   @Before
+   public void beforeClass() {
+      logger.info("-----테스트 시작-----");      
+   }
+   
+   @After
+   public void afterClass() {
+      logger.info("-----테스트 종료-----");
+   }
 
-	@Test
-	public void dbTest() {
+   @Test
+   public void dbTest() {
 
-		// 컨테이너에서 getBean()
-		SqlSessionTemplate session 
-		= wac.getBean("sqlSessoinTemplate",SqlSessionTemplate.class);
-		logger.info("session : " + session.toString());
-	}	
+      // 컨테이너에서 getBean()
+      SqlSessionTemplate session
+      = wac.getBean("sqlSessionTemplate",SqlSessionTemplate.class);
+      logger.info("session : " + session.toString());
+   }   
 }
--- src/test/java/com/kh/appTest/TestMemberController.java
+++ src/test/java/com/kh/appTest/TestMemberController.java
@@ -19,46 +19,46 @@ import org.springframework.web.context.WebApplicationContext;
 
 //SpringJUnit4ClassRunner는 스프링 테스트를 위한 Runner 클래스 지정
 @RunWith(SpringJUnit4ClassRunner.class)
-@WebAppConfiguration	// Controller및 web환경에 사용되는 bean들을 자동으로 생성하여 등록
+@WebAppConfiguration   // Controller및 web환경에 사용되는 bean들을 자동으로 생성하여 등록
 @ContextConfiguration(locations={"file:src/main/webapp/WEB-INF/spring/root-context.xml",
 "file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml"}) //자동으로 만들어줄 애플리케이션 컨텍스트의 설정 파일 경로를 지정
 public class TestMemberController {
-	
-	private static final Logger LOGGER = LoggerFactory.getLogger(TestMemberController.class);
-	// Logger : 로그 출력을 위한 log4j 객체
-	
-	// 테스트용 Class에서 사용될 필드 선언
-	@Autowired
-	private WebApplicationContext wac; // 현재 실행중인 애플리케이션의 구성을 제공하는 인터페이스
-	private MockMvc mockMvc;	// client 요청 내용을 controller에서 받아 처리하는 것과 같은 테스트를 진행할 수 있는 클래스.
+   
+   private static final Logger LOGGER = LoggerFactory.getLogger(TestMemberController.class);
+   // Logger : 로그 출력을 위한 log4j 객체
+   
+   // 테스트용 Class에서 사용될 필드 선언
+   @Autowired
+   private WebApplicationContext wac; // 현재 실행중인 애플리케이션의 구성을 제공하는 인터페이스
+   private MockMvc mockMvc;   // client 요청 내용을 controller에서 받아 처리하는 것과 같은 테스트를 진행할 수 있는 클래스.
 
-	
-	@Before // JUnit 테스트 진행 전 먼저 실행하는 것을 지정하는 어노테이션
-	public void setup() {
-		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
-		LOGGER.info("setup() 메소드 호출");
-	}
+   
+   @Before // JUnit 테스트 진행 전 먼저 실행하는 것을 지정하는 어노테이션
+   public void setup() {
+      this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+      LOGGER.info("setup() 메소드 호출");
+   }
 
 
-	@Test // 테스트용 메소드임을 명시하는 어노테이션
-	public void testmemberRegister() {
-		
-		LOGGER.info("테스트 시작");
-		try{
-			// 테스트 메소드 내부에 mockMvc를 이용하여 매핑될 url과 필요한 데이터가 담긴 가상의 요청을 작성.  (perfrom() 메소드)
+   @Test // 테스트용 메소드임을 명시하는 어노테이션
+   public void testmemberRegister() {
+      
+      LOGGER.info("테스트 시작");
+      try{
+         // 테스트 메소드 내부에 mockMvc를 이용하여 매핑될 url과 필요한 데이터가 담긴 가상의 요청을 작성.  (perfrom() 메소드)
 
-			mockMvc.perform(post("/member/memberEnroll.do")
-			.param("userid","test01")
-			.param("passwd","test01*")
-			.param("email","test01@gmail.com")
-			.param("tel","01023456789")  )
-			.andDo(print())  //처리되어진 내용을 출력 
-			.andExpect(status().isOk()); // 응답 상태값이 에러가 없는 정상적인 상태(status 가 200)가 되도록 검증 
-			
-			LOGGER.info("테스트 시작");
-			
-		} catch(Exception e){
-			e.printStackTrace();
-		}
-	}
+         mockMvc.perform(post("/member/memberEnroll.do")
+         .param("userid","test01")
+         .param("passwd","test01*")
+         .param("email","test01@gmail.com")
+         .param("tel","01023456789")  )
+         .andDo(print())  //처리되어진 내용을 출력 
+         .andExpect(status().isOk()); // 응답 상태값이 에러가 없는 정상적인 상태(status 가 200)가 되도록 검증 
+         
+         LOGGER.info("테스트 시작");
+         
+      } catch(Exception e){
+         e.printStackTrace();
+      }
+   }
 }
